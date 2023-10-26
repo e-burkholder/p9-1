be4af53ed83b78cfaa8d05fd111dd8b82f06c0d7
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-26 02:21:04 -0400

    element appearance in the array.

--- p9-1/Source.cpp
+++ p9-1/Source.cpp
@@ -1,1 +1,21 @@
-//p9-1
+#include <iostream>
+#include <ctime>
+#include <random>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	int randomDigits[10];
+	srand(time(0));
+
+	//generate 10 random integers
+	cout << "The random integers: ";
+	for (int i = 0; i < 10; i++) {
+		randomDigits[i] = rand() % 9000 + 1000;
+		cout << randomDigits[i] << " ";
+	}
+
+
+	return 0;
+}

77dd214b53c1343a7b24445deffee1b697a1f50e
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-26 02:29:15 -0400

    Every element at an even index.

--- p9-1/Source.cpp
+++ p9-1/Source.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
 	int randomDigits[10];
-	srand(time(0));
+	//srand(time(0));
 
 	//generate 10 random integers
 	cout << "The random integers: ";
@@ -16,6 +16,11 @@ int main() {
 		cout << randomDigits[i] << " ";
 	}
 
+	cout << "\nEven indices: ";
+	for (int i = 0; i < 9;) {
+		cout << randomDigits[i] << " ";
+		i += 2;
+	}
 
 	return 0;
 }
